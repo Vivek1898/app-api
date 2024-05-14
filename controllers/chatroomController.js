e056256317a20c34b20f30aad1fb2a5f65a95fef
vivek <viveksingh27795@gmail.com>
2024-05-14 18:20:52 +0530

    Fixes in room

--- controllers/chatroomController.js
+++ controllers/chatroomController.js
@@ -45,7 +45,17 @@ exports.getAllChatrooms = async (req, res) => {
             {userOne: request.userId},
             {userTwo: request.userId},
         ],
+    }).populate({
+        path: "userOne",
+        select: "name email location bio profilePicture jobProfile education experience jobCategory",
+    }).populate({
+        path: "userTwo",
+        select: "name email location bio profilePicture jobProfile education experience jobCategory",
+
     });
+
+    // populate userOne and userTwo
+
 
     return ResponseService.jsonResponse(res, ConstantService.responseCode.SUCCESS, {
         message: "Chatrooms fetched successfully",
@@ -54,68 +64,68 @@ exports.getAllChatrooms = async (req, res) => {
 };
 
 exports.createRoomForTwoUsers = async (req, res) => {
-  try {
-    const {userOne, userTwo} = req.body;
-    const schema = Joi.object({
-      userOne: Joi.string().required(),
-      userTwo: Joi.string().required()
-    });
+    try {
+        const {userOne, userTwo} = req.body;
+        const schema = Joi.object({
+            userOne: Joi.string().required(),
+            userTwo: Joi.string().required()
+        });
 
-    const {error} = schema.validate(req.body);
-    if (error) {
-      return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
-        message: error.message,
-      });
+        const {error} = schema.validate(req.body);
+        if (error) {
+            return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
+                message: error.message,
+            });
+        }
+        // check if userOne and userTwo are same
+        if (userOne === userTwo) {
+            return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
+                message: "UserOne and UserTwo cannot be same",
+            });
+        }
+
+        // check if both user exist in the system
+        const userOneExists = await User.findOne({
+            _id: userOne
+        });
+
+        const userTwoExists = await User.findOne({
+            _id: userTwo
+        });
+
+        if (_.isEmpty(userOneExists) || _.isEmpty(userTwoExists)) {
+            return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
+                message: "UserOne or UserTwo does not exist",
+            });
+        }
+
+        // slice last 6 characters from userOne and userTwo
+        const name = userOne.slice(-6) + userTwo.slice(-6);
+        const chatroomExists = await Chatroom.findOne({
+            name
+        })
+        if (!_.isEmpty(chatroomExists)) {
+            return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
+                message: "Chatroom already exists",
+            });
+        }
+        const chatroom = new Chatroom({
+            name,
+            userOne,
+            userTwo
+        });
+        await chatroom.save();
+
+        return ResponseService.jsonResponse(res, ConstantService.responseCode.SUCCESS, {
+            message: "Chatroom created successfully",
+            data: chatroom
+        });
+
+    } catch (e) {
+        console.error(e);
+        return ResponseService.json(res, ConstantService.responseCode.INTERNAL_SERVER_ERROR, ConstantService.responseMessage.ERR_OOPS_SOMETHING_WENT_WRONG_IN_CREATING_CHATROOM);
+
     }
-    // check if userOne and userTwo are same
-    if (userOne === userTwo) {
-      return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
-        message: "UserOne and UserTwo cannot be same",
-      });
-    }
-
-    // check if both user exist in the system
-    const userOneExists = await User.findOne({
-      _id: userOne
-    });
-
-    const userTwoExists = await User.findOne({
-      _id: userTwo
-    });
-
-    if (_.isEmpty(userOneExists) || _.isEmpty(userTwoExists)) {
-      return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
-        message: "UserOne or UserTwo does not exist",
-      });
-    }
-
-    // slice last 6 characters from userOne and userTwo
-    const name = userOne.slice(-6) + userTwo.slice(-6);
-    const chatroomExists = await Chatroom.findOne({
-      name
-    })
-    if (!_.isEmpty(chatroomExists)) {
-      return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
-        message: "Chatroom already exists",
-      });
-    }
-    const chatroom = new Chatroom({
-      name,
-      userOne,
-      userTwo
-    });
-    await chatroom.save();
-
-    return ResponseService.jsonResponse(res, ConstantService.responseCode.SUCCESS, {
-      message: "Chatroom created successfully",
-      data: chatroom
-    });
-
-  } catch (e) {
-    console.error(e);
-    return ResponseService.json(res, ConstantService.responseCode.INTERNAL_SERVER_ERROR, ConstantService.responseMessage.ERR_OOPS_SOMETHING_WENT_WRONG_IN_CREATING_CHATROOM);
-
-  }
 
 
 }
@@ -136,9 +146,15 @@ exports.getChatRoomDetails = async (req, res) => {
             });
         }
 
-        const chatroom = await Chatroom.findOne({ _id: chatRoomId })
-            .populate({ path: 'userOne', select: 'name email location bio profilePicture jobProfile education experience jobCategory' })
-            .populate({ path: 'userTwo', select: 'name email location bio profilePicture jobProfile education experience jobCategory' });
+        const chatroom = await Chatroom.findOne({_id: chatRoomId})
+            .populate({
+                path: 'userOne',
+                select: 'name email location bio profilePicture jobProfile education experience jobCategory'
+            })
+            .populate({
+                path: 'userTwo',
+                select: 'name email location bio profilePicture jobProfile education experience jobCategory'
+            });
 
         if (_.isEmpty(chatroom)) {
             return ResponseService.jsonResponse(res, ConstantService.responseCode.BAD_REQUEST, {
